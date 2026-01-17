7d442ecf301a3087c6c56da0b5f5fa6ec9ba1de1
Tasnim Munawar Rafee <rafi00001111@outlook.com>
2026-01-17 16:58:55 +0600

    ..

--- resources/views/admin-views/report/stock-report.blade.php
+++ resources/views/admin-views/report/stock-report.blade.php
@@ -8,82 +8,85 @@
 
 @section('content')
 
-    <div class="content container-fluid">
-        <!-- Page Header -->
-        <div class="page-header">
-            <div class="row align-items-center">
-                <div class="col-12 mb-2 mb-sm-0">
-                    <h1 class="page-header-title"><i class="tio-filter-list"></i> {{translate('Stock report')}}</h1>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-4 col-12 mb-2">
-                    <select name="module_id" class="form-control js-select2-custom" onchange="set_filter('{{url()->full()}}',this.value,'module_id')" title="{{translate('messages.select')}} {{translate('messages.modules')}}">
-                        <option value="" {{!request('module_id') ? 'selected':''}}>{{translate('messages.all')}} {{translate('messages.modules')}}</option>
-                        @foreach (\App\Models\Module::notParcel()->get() as $module)
-                            @if (config('module.'.$module->module_type)['stock'])
-                                <option
-                                    value="{{$module->id}}" {{request('module_id') == $module->id?'selected':''}}>
-                                    {{$module['module_name']}}
-                                </option>                                
-                            @endif
-
-                        @endforeach
-                    </select>
-                </div>
-                <div class="col-sm-4 col-12 mb-2">
-                    <select name="zone_id" class="form-control js-select2-custom"
-                            onchange="set_zone_filter('{{url()->full()}}',this.value)" id="zone">
-                        <option value="all">{{translate('All Zones')}}</option>
-                        @foreach(\App\Models\Zone::orderBy('name')->get() as $z)
-                            <option
-                                value="{{$z['id']}}" {{isset($zone) && $zone->id == $z['id']?'selected':''}}>
-                                {{($z['name'])}}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="col-sm-4 col-12">
-                    <select name="store_id" onchange="set_store_filter('{{url()->full()}}',this.value)" data-placeholder="{{translate('messages.select')}} {{translate('messages.store')}}" class="js-data-example-ajax form-control">
-                        @if(isset($store))    
-                        <option value="{{$store->id}}" selected>{{$store->name}}</option>
-                        @else
-                        <option value="all" selected>{{translate('messages.all')}} {{translate('messages.stores')}}</option>
-                        @endif
-                    </select>
-                </div>
+<div class="content container-fluid">
+    <!-- Page Header -->
+    <div class="page-header">
+        <div class="row align-items-center">
+            <div class="col-12 mb-2 mb-sm-0">
+                <h1 class="page-header-title"><i class="tio-filter-list"></i> {{translate('Stock report')}}</h1>
             </div>
         </div>
-        <!-- End Page Header -->
-        <!-- Card -->
-        <div class="row card mt-4">
-            <!-- Header -->
-            <div class="card-header py-0">
-                <div class="row justify-content-between align-items-center flex-grow-1">
-                    <div class="col-md-4 mb-3 mb-md-0">
-                        <form id="search-form">
+        <div class="row">
+            <div class="col-sm-4 col-12 mb-2">
+                <select name="module_id" class="form-control js-select2-custom" onchange="set_filter('{{url()->full()}}',this.value,'module_id')" title="{{translate('messages.select')}} {{translate('messages.modules')}}">
+                    <option value="" {{!request('module_id') ? 'selected':''}}>{{translate('messages.all')}} {{translate('messages.modules')}}</option>
+                    @foreach (\App\Models\Module::notParcel()->get() as $module)
+                    @if (config('module.'.$module->module_type)['stock'])
+                    <option
+                        value="{{$module->id}}" {{request('module_id') == $module->id?'selected':''}}>
+                        {{$module['module_name']}}
+                    </option>
+                    @endif
+
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-sm-4 col-12 mb-2">
+                <select name="zone_id" class="form-control js-select2-custom"
+                    onchange="set_zone_filter('{{url()->full()}}',this.value)" id="zone">
+                    <option value="all">{{translate('All Zones')}}</option>
+                    @foreach(\App\Models\Zone::orderBy('name')->get() as $z)
+                    <option
+                        value="{{$z['id']}}" {{isset($zone) && $zone->id == $z['id']?'selected':''}}>
+                        {{($z['name'])}}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-sm-4 col-12">
+                <select name="store_id" onchange="set_store_filter('{{url()->full()}}',this.value)" data-placeholder="{{translate('messages.select')}} {{translate('messages.store')}}" class="js-data-example-ajax form-control">
+                    @if(isset($store))
+                    <option value="{{$store->id}}" selected>{{$store->name}}</option>
+                    @else
+                    <option value="all" selected>{{translate('messages.all')}} {{translate('messages.stores')}}</option>
+                    @endif
+                </select>
+            </div>
+        </div>
+    </div>
+    <!-- End Page Header -->
+    <!-- Card -->
+    <div class="row card mt-4">
+        <!-- Header -->
+        <div class="card-header py-0">
+            <div class="row justify-content-between align-items-center flex-grow-1">
+                <div class="col-md-4 mb-3 mb-md-0">
+                    <form id="search-form">
                         <!-- Search -->
                         <div class="input-group input-group-merge input-group-flush">
                             <div class="input-group-prepend">
-                            <div class="input-group-text">
-                                <i class="tio-search"></i>
-                            </div>
+                                <div class="input-group-text">
+                                    <i class="tio-search"></i>
+                                </div>
                             </div>
                             <input id="datatableSearch" name="search" type="search" class="form-control" placeholder="{{translate('messages.search_here')}}" aria-label="{{translate('messages.search_here')}}" value="{{request()->query('search')}}">
                             <button type="submit" class="btn btn-light">{{translate('messages.search')}}</button>
                         </div>
                         <!-- End Search -->
-                        </form>
-                    </div>
+                    </form>
                 </div>
-                <!-- End Row -->
             </div>
-            <!-- End Header -->
+            <!-- End Row -->
+        </div>
+        <!-- End Header -->
+        <script>
+            var $s = "{{ asset('assets/admin/img/160x160/img2.jpg') }}";
+        </script>
 
-            <!-- Table -->
-            <div class="table-responsive datatable-custom" id="table-div">
-                <table id="datatable" class="table table-borderless table-thead-bordered table-nowrap card-table"
-                    data-hs-datatables-options='{
+        <!-- Table -->
+        <div class="table-responsive datatable-custom" id="table-div">
+            <table id="datatable" class="table table-borderless table-thead-bordered table-nowrap card-table"
+                data-hs-datatables-options='{
                         "columnDefs": [{
                             "targets": [],
                             "width": "5%",
@@ -100,7 +103,7 @@
                         "isShowPaging": false,
                         "paging":false
                     }'>
-                    <thead class="thead-light">
+                <thead class="thead-light">
                     <tr>
                         <th>{{translate('messages.#')}}</th>
                         <th style="width: 20%">{{translate('messages.name')}}</th>
@@ -109,61 +112,65 @@
                         <th>{{translate('Current stock')}}</th>
                         <th>{{translate('messages.action')}}</th>
                     </tr>
-                    </thead>
+                </thead>
 
-                    <tbody id="set-rows">
-                    
+                <tbody id="set-rows">
+
                     @foreach($items as $key=>$item)
-                        <tr>
-                            <td>{{$key+$items->firstItem()}}</td>
-                            <td>
-                                <a class="media align-items-center" href="{{route('admin.item.view',[$item['id']])}}">
-                                    <img class="avatar avatar-lg mr-3" src="{{asset('storage/product')}}/{{$item['image']}}" 
-                                            onerror="this.src='{{asset('assets/admin/img/160x160/img2.jpg')}}'" alt="{{$item->name}} image">
-                                    <div class="media-body">
-                                        <h5 class="text-hover-primary mb-0">{{$item['name']}}</h5>
-                                    </div>
-                                </a>
-                            </td>
-                            <td>
-                                @if($item->store)
-                                {{Str::limit($item->store->name,25,'...')}}
-                                @else
-                                {{translate('messages.store')}} {{translate('messages.deleted')}}
-                                @endif
-                            </td>
-                            <td>
-                                @if($item->store)
-                                    {{$item->store->zone->name}}
-                                @else
-                                    {{translate('messages.not_found')}}
-                                @endif
-                            </td>
-                            <td>
-                                {{$item->stock}}
-                            </td>
-                            <td>
-                                <a class="btn btn-sm btn-white"
-                                    href="{{route('admin.item.edit',[$item['id']])}}" title="{{translate('messages.edit')}} {{translate('messages.item')}}"><i class="tio-edit"></i>
-                                </a>
-                            </td>
-                        </tr>
+                    <tr>
+                        <td>{{$key+$items->firstItem()}}</td>
+                        <td>
+                            <a class="media align-items-center" href="{{route('admin.item.view',[$item['id']])}}">
+                                <img class="avatar avatar-lg mr-3"
+                                    src="{{ asset('storage/product/' . $item->image) }}"
+                                    onerror="this.onerror=null;this.src='{{ $s }}'"
+                                    alt="{{ $item->name }} image">
+
+
+                                <div class="media-body">
+                                    <h5 class="text-hover-primary mb-0">{{$item['name']}}</h5>
+                                </div>
+                            </a>
+                        </td>
+                        <td>
+                            @if($item->store)
+                            {{Str::limit($item->store->name,25,'...')}}
+                            @else
+                            {{translate('messages.store')}} {{translate('messages.deleted')}}
+                            @endif
+                        </td>
+                        <td>
+                            @if($item->store)
+                            {{$item->store->zone->name}}
+                            @else
+                            {{translate('messages.not_found')}}
+                            @endif
+                        </td>
+                        <td>
+                            {{$item->stock}}
+                        </td>
+                        <td>
+                            <a class="btn btn-sm btn-white"
+                                href="{{route('admin.item.edit',[$item['id']])}}" title="{{translate('messages.edit')}} {{translate('messages.item')}}"><i class="tio-edit"></i>
+                            </a>
+                        </td>
+                    </tr>
                     @endforeach
-                    </tbody>
-                </table>
-                <hr>
-                <div class="page-area">
-                    <table>
-                        <tfoot class="border-top">
+                </tbody>
+            </table>
+            <hr>
+            <div class="page-area">
+                <table>
+                    <tfoot class="border-top">
                         {!! $items->links() !!}
-                        </tfoot>
-                    </table>
-                </div>
+                    </tfoot>
+                </table>
             </div>
-            <!-- End Table -->
         </div>
-        <!-- End Card -->
+        <!-- End Table -->
     </div>
+    <!-- End Card -->
+</div>
 @endsection
 
 @push('script')
@@ -172,47 +179,55 @@
 
 @push('script_2')
 
-    <script src="{{asset('public/assets/admin')}}/vendor/chart.js/dist/Chart.min.js"></script>
-    <script
-        src="{{asset('public/assets/admin')}}/vendor/chartjs-chart-matrix/dist/chartjs-chart-matrix.min.js"></script>
-    <script src="{{asset('public/assets/admin')}}/js/hs.chartjs-matrix.js"></script>
+<script src="{{asset('public/assets/admin')}}/vendor/chart.js/dist/Chart.min.js"></script>
+<script
+    src="{{asset('public/assets/admin')}}/vendor/chartjs-chart-matrix/dist/chartjs-chart-matrix.min.js"></script>
+<script src="{{asset('public/assets/admin')}}/js/hs.chartjs-matrix.js"></script>
 
-    <script>
-        $(document).on('ready', function () {
-            $('.js-data-example-ajax').select2({
-                ajax: {
-                    url: '{{url('/')}}/admin/vendor/get-stores',
-                    data: function (params) {
-                        return {
-                            q: params.term, // search term
-                            // all:true,
-                    @if(isset($zone))zone_ids: [{{$zone->id}}], @endif
-                    @if(request('module_id'))module_id: {{request('module_id')}}, @endif
-                            page: params.page
-                        };
-                    },
-                    processResults: function (data) {
-                        return {
+<script>
+    $(document).ready(function() {
+        $('.js-data-example-ajax').select2({
+            ajax: {
+                url: "{{ url('/admin/vendor/get-stores') }}",
+                dataType: 'json',
+                delay: 250,
+                data: function(params) {
+                    let query = {
+                        q: params.term || '',
+                        page: params.page || 1,
+                        @if(isset($zone))
+                        zone_ids: [{
+                            {
+                                $zone - > id
+                            }
+                        }],
+                        @endif
+                        @if(request() - > has('module_id'))
+                        module_id: {
+                            {
+                                request('module_id')
+                            }
+                        },
+                        @endif
+                    };
+                    return query;
+                },
+                processResults: function(data) {
+                    return {
                         results: data
-                        };
-                    },
-                    __port: function (params, success, failure) {
-                        var $request = $.ajax(params);
-
-                        $request.then(success);
-                        $request.fail(failure);
-
-                        return $request;
-                    }
-                }
-            });
+                    };
+                },
+                cache: true
+            }
         });
-    </script>
+    });
+</script>
 
-    <script>        
-        $('#search-form').on('submit', function (e) {
-            e.preventDefault();
-            set_filter('{!! url()->full() !!}', $('#datatableSearch').val(), 'search');
-        });
-    </script>
+
+<script>
+    $('#search-form').on('submit', function(e) {
+        e.preventDefault();
+        set_filter('{!! url()->full() !!}', $('#datatableSearch').val(), 'search');
+    });
+</script>
 @endpush
