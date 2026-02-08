681c910eefef253d03bedaec9c95aced30f69277
Tasnim Munawar Rafee <rafi00001111@outlook.com>
2026-02-08 18:59:32 +0600

    Update ModuleMiddleware.php

--- app/Http/Middleware/ModuleMiddleware.php
+++ app/Http/Middleware/ModuleMiddleware.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use App\Models\BusinessSetting;
+
 class ModuleMiddleware
 {
     /**
@@ -19,26 +20,47 @@ class ModuleMiddleware
      */
 
     public function handle($request, Closure $next, $module)
-{
-    $admin = auth('admin')->user();
+    {
 
-    if (!$admin) {
+        if (auth('vendor')->check()) {
+            $vendor = auth('vendor')->user();
+
+            // vendor active check
+            if ($vendor->status != 1) {
+                abort(403, 'Vendor not active');
+            }
+
+            return $next($request);
+        }
+
+        if (auth('admin')->check()) {
+            $admin = auth('admin')->user();
+
+            if ($admin->role_id == 1) {
+                return $next($request);
+            }
+        }
+
         abort(403);
+
+
+        $admin = auth('admin')->user();
+
+        if (!$admin) {
+            abort(403);
+        }
+
+        // SUPER ADMIN BYPASS
+        if ($admin->role_id == 1) {
+            return $next($request);
+        }
+
+        $setting = BusinessSetting::where('key', $module)->first();
+
+        if (!$setting || $setting->value != 1) {
+            abort(403);
+        }
+
+        abort(403); // other roles blocked for now
     }
-
-    // SUPER ADMIN BYPASS
-    if ($admin->role_id == 1) {
-        return $next($request);
-    }
-
-    $setting = BusinessSetting::where('key', $module)->first();
-
-    if (!$setting || $setting->value != 1) {
-        abort(403);
-    }
-
-    abort(403); // other roles blocked for now
-}
-
-
 }
